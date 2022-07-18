390c7af8fb3ff807e2dbf6176786c4a69ef747a1
Will Schmidt <will@willschmidt.ca>
2022-07-18 07:56:24 -0600

    Added SG and Volume output

--- search.js
+++ search.js
@@ -19,13 +19,20 @@ program
   .option('-i, --instance <string>', 'Instance', undefined)
   .option('-n, --name <string>', 'Server Name', undefined)
   .option('-d, --detailed', 'Display Instance Information in Console')
+  .option('-vol --volumes', 'Display Volume information')
+  .option('-sg, --securitygroups', 'Display Security Group Info')
   .parse();
 
 // Parse arguments and populate options
 program.parse();
 const options = program.opts();
 
-if(options.human) {
+//Generate Header
+if (options.volumes) {
+  process.stdout.write(`Account\t AccountID\t AZ\t Server\t InstanceID\t Device\t VolumeID\t AttachTime\n`)
+} else if (options.securitygroups) {
+  process.stdout.write(`Account\t AccountID\t AZ\t Server\t InstanceID\t VPC\t SG Name\t SG ID\n`)
+} else if (options.detailed) {
   process.stdout.write(`Scanning ${instances.length} instances`);
 } else {
   process.stdout.write(`Account\t AccountID\t AZ\t Server\t InstanceID\t IP\t Platform\t InstanceType\n`)
@@ -37,16 +44,25 @@ let results = [];
 instances.forEach(instance => {
   if((!options.instance && !options.name) || (options.instance && instance.InstanceId.toLowerCase().includes(options.instance.toLowerCase())) || (options.name && instance.Name.toLowerCase().includes(options.name.toLowerCase()))) {
     if(JSON.stringify(instance).toLowerCase().includes(options.search.toLowerCase())) {
-      if (!options.detailed) {
-        process.stdout.write(`${instance.Account}\t ${instance.AccountID}\t ${instance.AZ}\t ${instance.Name}\t ${instance.InstanceId}\t ${instance.Ip}\t ${instance.Platform}\t ${instance.InstanceType}\n`);
+      if (options.volumes) {
+        //Get all block devices
+        const blockDevices = instance.blockdevices;
+        blockDevices.forEach(disk => {
+          process.stdout.write(`${instance.Account}\t ${instance.AccountID}\t ${instance.AZ}\t ${instance.Name}\t ${instance.InstanceId}\t ${disk.DeviceName}\t ${disk.Ebs.VolumeId}\t ${disk.Ebs.AttachTime} \n`)  
+        })
+        
+      } else if (options.securitygroups) {
+        //Get Security Groups
+        const securityGroups = instance.securityGroups
+        securityGroups.forEach(sg => {
+          process.stdout.write(`${instance.Account}\t ${instance.AccountID}\t ${instance.AZ}\t ${instance.Name}\t ${instance.InstanceId}\t ${instance.VPC}\t ${sg.GroupName}\t ${sg.GroupId} \n`)
+        })
+
+      } else if (options.detailed) {
+        process.stdout.write(JSON.stringify(instance,null,2))
       } else {
-        results.push(instance);
-      }
-      
+        process.stdout.write(`${instance.Account}\t ${instance.AccountID}\t ${instance.AZ}\t ${instance.Name}\t ${instance.InstanceId}\t ${instance.Ip}\t ${instance.Platform}\t ${instance.InstanceType}\n`);
+      }     
     }
   }
 });
-
-if(options.detailed) {
-  process.stdout.write(JSON.stringify(results,null,2));
-}
